2a894b63be92d1fafe352cfa1d682415339bb80e
Vitaly Kot <90048750+vitaliycatt@users.noreply.github.com>
2022-09-21 14:03:44 +0300

    Adding second page and router to them (#1)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import styles from './app.module.css'
+import { Home } from './pages/home'
+import { About } from './pages/about'
+
 export const App = () => {
-  return <div>Main page</div>
+  return (
+    <div className={styles.container}>
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </div>
+  )
 }
